1fb6f7b972f14e5299edf0d67efefd28868111e5
Svitlana Pushchalo <svitlana8@gmail.com>
2021-09-29 20:24:37 +0300

    Initial version

--- tests/nitrosalt_test.py
+++ tests/nitrosalt_test.py
@@ -1,9 +1,9 @@
 import unittest
 import sys
 import os
+from testing.main_ import *
 # у модуля sys є атрибут path - список з усіма директоріями системними і оск це список, то він має метод append
 sys.path.append(os.getcwd())
-from main_ import *
 
 
 class TestNitroSalt(unittest.TestCase):
@@ -22,12 +22,11 @@ class TestNitroSalt(unittest.TestCase):
         self.assertEqual(nitro_salt("hfghf"), 0)
 
 
-
 if __name__ == '__main__':
     unittest.main()
     # метод main() у unittest, який азапускає всі наші тести
 
 # оскільки наш файл (модуль) з кодом main_.py знаходиться на рівень вище, ніж тести,
 # то виникає проблема імпортування всіх функцій, сутностей із файлу main_.py у файл test.py
-# щоб імпортувати модуль, який знаходиться на рівень вище, потрібно додати папку testing у системну змінну path
+# щоб імпортувати модуль, який знаходиться на рівень вище, потрібно додати папку testing_unittesting у системну змінну path
 # і для цього ми імпортуємо ще два модулі: import sys, import os
