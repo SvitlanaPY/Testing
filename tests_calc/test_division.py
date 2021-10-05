5a0068a0a36ec65828aa3a715a2d8f7cbbe3c770
Svitlana Pushchalo <svitlana8@gmail.com>
2021-10-05 15:35:23 +0300

    changes

--- tests_calc/test_division.py
+++ tests_calc/test_division.py
@@ -38,14 +38,19 @@ class TestDiv(unittest.TestCase):
 if __name__ == '__main__':
     unittest.main()
 
-# if there is unittest.main() in a python-file then run command:
-# ~$ python3 test_division.py
-# (функція main() у unittest запустить наш файл test_division.py з тестами як окремий модуль напряму:
-# і наш модуль буде мати імя "main": __name__ == '__main__'
+# якщо у модулі є блок:
+# if __name__ == '__main__':
+#     unittest.main()
+# то наш модуль test_division.py можна запустити в терміналі напряму, командою: ~$ python3 test_division.py
+# ця команда рівносильна запуску тестів через меню Run, це запуск модуля самостійно, а НЕ як імпортованого модуля
+# (при такому запуску if __name__ == '__main__' і функція main() у unittest запустить наш файл test_division.py з тестами як окремий модуль напряму)
 # or
-# command ~$ python3 -m unittest -v test_division.py > output_unittesting.txt
+# запускаючи тести командою ~$ python3 -m unittest -v test_division.py > output_unittesting.txt
 # ми вказуємо інтерпретатору пайтон запустити наш модуль test_division.py з тестами з бібліотеки unittest як імпортованого модуля
-# тобто тепер наш модуль втрачає імя "main", а набуває імені модуля: __name__ == "test_division",
-# оск він імпортується і запускається з бібліотеки unittest.
-
+# ця команда - це скорочення для командної стрічки для запуску метода main() з бібліотеки unittest;
+# при такому запуску наш модуль втрачає імя "main", а набуває імені модуля: __name__ == "test_division",
+# оск він імпортується і запускається з бібліотеки unittest, і блок if ... не буде виконуватись:
+# if __name__ == '__main__':
+#     unittest.main()
+# тепер сама бібліотека unittest буде знати, що робити і як запустити імпортований модуль;
 # results will be saved in file 'output_unittesting.txt'
--- tests_calc/test_sum.py
+++ tests_calc/test_sum.py
@@ -37,14 +37,19 @@ if __name__ == '__main__':
     unittest.main()
 
 
-# if there is unittest.main() in a python-file then run command:
-# ~$ python3 test_sum.py
-# (функція main() у unittest запустить наш файл test_sum.py з тестами як окремий модуль напряму:
-# і наш модуль буде мати імя "main": __name__ == '__main__'
+# якщо у модулі є блок:
+# if __name__ == '__main__':
+#     unittest.main()
+# то наш модуль test_division.py можна запустити в терміналі напряму, командою: ~$ python3 test_sum.py
+# ця команда рівносильна запуску тестів через меню Run, це запуск модуля самостійно, а НЕ як імпортованого модуля
+# (при такому запуску if __name__ == '__main__' і функція main() у unittest запустить наш файл test_sum.py з тестами як окремий модуль напряму)
 # or
-# command ~$ python3 -m unittest -v test_sum.py > output_unittesting.txt
+# запускаючи тести командою ~$ python3 -m unittest -v test_sum.py > output_unittesting.txt
 # ми вказуємо інтерпретатору пайтон запустити наш модуль test_sum.py з тестами з бібліотеки unittest як імпортованого модуля
-# тобто тепер наш модуль втрачає імя "main", а набуває імені модуля: __name__ == "test_sum",
-# оск він імпортується і запускається з бібліотеки unittest.
-
+# ця команда - це скорочення для командної стрічки для запуску метода main() з бібліотеки unittest;
+# при такому запуску наш модуль втрачає імя "main", а набуває імені модуля: __name__ == "test_sum",
+# оск він імпортується і запускається з бібліотеки unittest, і блок if ... не буде виконуватись:
+# if __name__ == '__main__':
+#     unittest.main()
+# тепер сама бібліотека unittest буде знати, що робити і як запустити імпортований модуль test_sum;
 # results will be saved in file 'output_unittesting.txt'
